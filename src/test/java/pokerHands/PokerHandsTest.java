19ec87d20c63e88086c5fe00b3f21d1ea77f4c3a
Vicki <458044962@qq.com>
2020-08-26 15:31:41 +0800

    [pokerHands][cendeal vicki]test: add should_return_Tie_when_handleHighCard_given_1H2D3S4CKD_and_5H6D7S8CKD test

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -16,4 +16,17 @@ public class PokerHandsTest {
         //then
         assertEquals("White wins. - with high card: Ace", result);
     }
+
+    @Test
+    public void should_return_Tie_when_handleHighCard_given_1H2D3S4CKD_and_5H6D7S8CKD() {
+        //given
+        String[] black = {"2H", "3D", "5S", "9C", "KD"};
+        String[] white = {"2H", "3D", "5S", "9C", "KD"};
+        //when
+        PokerHands pokerHands = new PokerHands();
+        String result = pokerHands.handleHighCard(black, white);
+        //then
+        assertEquals("Tie", result);
+
+    }
 }
