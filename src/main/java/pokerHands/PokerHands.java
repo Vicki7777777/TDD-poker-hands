7c4223050f733adcec98ec5a4b6aa6d6a6f51aaa
Vicki <458044962@qq.com>
2020-08-26 15:28:39 +0800

    [pokerHands][cendeal vicki]feature: implement should_return_white_wins_when_handleHighCard_given_2H3D5S9CKD_and_2C3H4S8CAH

--- src/main/java/pokerHands/PokerHands.java
+++ src/main/java/pokerHands/PokerHands.java
@@ -1,7 +1,31 @@
 package pokerHands;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class PokerHands {
+    String cards= "23456789TJQKA";
+    String[] cardNames = {
+            "Jack","Queen","King","Ace"
+    };
     public String handleHighCard(String[] black, String[] white) {
-        return null;
+        List<Integer> blackFirsts = Arrays.stream(black).map(card->cards.indexOf(card.charAt(0))+1).collect(Collectors.toList());
+        List<Integer> whiteFirsts = Arrays.stream(white).map(card->cards.indexOf(card.charAt(0))+1).collect(Collectors.toList());
+        Integer blackMax = blackFirsts.stream().max(Integer::compare).orElse(-1);
+        Integer whiteMax = whiteFirsts.stream().max(Integer::compare).orElse(-1);
+        String winner = "Black";
+        Integer max = blackMax;
+        if(blackMax<whiteMax){
+            winner = "White";
+            max = whiteMax;
+        }
+        String cardName = max.toString();
+        if(max>10){
+            cardName = cardNames[max-10];
+        }
+        return String.format("%s wins. - with high card: %s", winner,cardName);
     }
 }
