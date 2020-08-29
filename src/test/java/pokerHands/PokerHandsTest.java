5e3e8035f8eba5adaf34b0d7f207ee8b1e1c10a1
Vicki <458044962@qq.com>
2020-08-29 19:11:13 +0800

    [pokerHands][cendeal vicki]test: should_white_win_when_handleFullHouse_given_3H3D3C6H6D_3H3D3C5H5D test

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -232,4 +232,15 @@ public class PokerHandsTest {
         assertEquals("Tie", result);
     }
 
+    @Test
+    public void should_white_win_when_handleFullHouse_given_3H3D3C6H6D_3H3D3C5H5D() {
+        //given
+        String[] white = {"3H", "3D", "3C", "6H", "6D"};
+        String[] black = {"3H", "3D", "3C", "5H", "5D"};
+        //when
+        PokerHands pokerHands = new PokerHands();
+        String result = pokerHands.handleFullHouse(black, white);
+        //then
+        assertEquals("White wins. - with full house: Three over Six", result);
+    }
 }
