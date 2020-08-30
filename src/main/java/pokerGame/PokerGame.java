89fccd036dcc0ee9c9bf2d5c7d0ad930b8ffb35e
Vicki <458044962@qq.com>
2020-08-30 17:32:52 +0800

    [pokerHands][cendeal vicki]refactor: fix some bugs in isThreeOfAKind method

--- src/main/java/pokerGame/PokerGame.java
+++ src/main/java/pokerGame/PokerGame.java
@@ -38,8 +38,16 @@ public class PokerGame {
 
     private boolean isThreeOfAKind(String[] pokers) {
         List<Integer> firstNumbers = getFirstNumber(pokers);
-        Set<Integer> firstNumbersSet = new HashSet<>(firstNumbers);
-        return firstNumbersSet.size() < 4;
+        int[] buckets = new int[cards.length()];
+        for(Integer value : firstNumbers){
+            buckets[value]++;
+        }
+        for(int i = 0;i<buckets.length-1;i++){
+            if(buckets[i] == 3){
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isStraight(String[] pokers) {
