fc327401ac239138d2e25a3931622ee6dc965196
Vicki <458044962@qq.com>
2020-08-30 13:24:53 +0800

    [pokerHands][cendeal vicki]feature: implement void should_return_string_array_when_handleInput_given_2H3D5S9CKD_and_2H3H4S8CAH

--- src/main/java/inputMessage/InputMessage.java
+++ src/main/java/inputMessage/InputMessage.java
@@ -3,6 +3,12 @@ package inputMessage;
 public class InputMessage {
 
     public String[] handleInput(String input) {
-        return null;
+        String[] inputArray = new String[5];
+        inputArray[0] = input.substring(0,2);
+        inputArray[1] = input.substring(2,4);
+        inputArray[2] = input.substring(4,6);
+        inputArray[3] = input.substring(6,8);
+        inputArray[4] = input.substring(8,10);
+        return inputArray;
     }
 }
